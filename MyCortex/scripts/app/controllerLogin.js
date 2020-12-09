6efaa5ddb670148c85301265a4b1991dbf1e975d
gowthamnri <gowtham848@gmail.com>
2020-12-09 13:43:44 +0530

    changes

--- MyCortex/scripts/app/controllerLogin.js
+++ MyCortex/scripts/app/controllerLogin.js
@@ -463,9 +463,9 @@ function ($scope, $http, $routeParams, $location, $rootScope, $window, $filter, 
                 $scope.ConfigCode = "WEBIDLETIME";
                 var IdleDays = 0;
                 $http.get(baseUrl + '/api/Common/AppConfigurationDetails/?ConfigCode=' + $scope.ConfigCode + '&Institution_Id=' + $window.localStorage['InstitutionId'])
-                    .success(function (data) {
-                        if (data[0] != undefined) {
-                            IdleDays = parseInt(data[0].ConfigValue);
+                    .success(function (data1) {
+                        if (data1[0] != undefined) {
+                            IdleDays = parseInt(data1[0].ConfigValue);
                             $window.localStorage['IdleDays'] = IdleDays;
                             if (data == "1") {
                                 $scope.errorlist = "Username and/or Password are not matching, please verify";
