e6a524c104bd632bfe703b0785c549cfc3224b4e
gowthamnri <gowtham848@gmail.com>
2021-06-18 18:01:43 +0530

    API Change

--- MyCortex/Repositories/IPatientAppointmentsRepository.cs
+++ MyCortex/Repositories/IPatientAppointmentsRepository.cs
@@ -16,6 +16,6 @@ namespace MyCortex.Repositories
         IList<PatientAppointmentsModel> DepartmentwiseDoctorList(long DepartmentId, long InstitutionId);
         IList<AppointmentReasonType> AppointmentReasonType_List(long Institution_Id);
         IList<ScheduledDaysListModel> GetScheduledDates(long TimezoneId, Guid Login_Session_Id);
-        IList<DoctorAppointmentTimeSlotModel> GetAppointmentTimeSlots(long DoctorId, long TimezoneId,DateTime Date, bool IsNew, Guid Login_Session_Id);
+        IList<DoctorAppointmentTimeSlotModel> GetAppointmentTimeSlots(long DoctorId, long TimezoneId,DateTime Date, int IsNew, Guid Login_Session_Id);
     }   
 }
