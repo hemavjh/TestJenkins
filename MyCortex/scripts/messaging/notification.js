259d5e10e5ac5be5864089d6b85cc150066dd1f3
gowthamnri <gowtham848@gmail.com>
2021-08-31 21:49:07 +0530

    Firebase config

--- MyCortex/scripts/messaging/notification.js
+++ MyCortex/scripts/messaging/notification.js
@@ -1,109 +1,109 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyBQRI1TZ3k9f13rtedqMtz0Ef8jtgYCeOY",
-    authDomain: "mycortex-2b207.firebaseapp.com",
-    databaseURL: "https://mycortex-2b207.firebaseio.com",
-    projectId: "mycortex-2b207",
-    storageBucket: "mycortex-2b207.appspot.com",
-    messagingSenderId: "538725094029",
-    appId: "1:538725094029:web:e1de3f6e70fd5bf464c8c4",
-    measurementId: "G-PYBLJGPE0S"
-};
+////var firebaseConfig = {
+////    apiKey: "AIzaSyBQRI1TZ3k9f13rtedqMtz0Ef8jtgYCeOY",
+////    authDomain: "mycortex-2b207.firebaseapp.com",
+////    databaseURL: "https://mycortex-2b207.firebaseio.com",
+////    projectId: "mycortex-2b207",
+////    storageBucket: "mycortex-2b207.appspot.com",
+////    messagingSenderId: "538725094029",
+////    appId: "1:538725094029:web:e1de3f6e70fd5bf464c8c4",
+////    measurementId: "G-PYBLJGPE0S"
+////};
 
 jQuery.get(baseUrl + '/api/Common/AppConfigurationDetails/?ConfigCode=FIREBASE_CONFIG&Institution_Id=' + window.localStorage.getItem('InstitutionId'))
-        .done(function (data) {
-            var jsonobj = jQuery.parseJSON(data[0].ConfigValue);
-            // Your web app's Firebase configuration
-            firebaseConfig = {
-                apiKey: jsonobj.apiKey,
-                authDomain: jsonobj.authDomain,
-                databaseURL: jsonobj.databaseURL,
-                projectId: jsonobj.projectId,
-                storageBucket: jsonobj.storageBucket,
-                messagingSenderId: jsonobj.messagingSenderId,
-                appId: jsonobj.appId,
-                measurementId: jsonobj.measurementId
-            };
-            firebase.initializeApp(firebaseConfig);
-            firebase.analytics();
+    .done(function (data) {
+        var jsonobj = jQuery.parseJSON(data[0].ConfigValue);
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: jsonobj.apiKey,
+            authDomain: jsonobj.authDomain,
+            databaseURL: jsonobj.databaseURL,
+            projectId: jsonobj.projectId,
+            storageBucket: jsonobj.storageBucket,
+            messagingSenderId: jsonobj.messagingSenderId,
+            appId: jsonobj.appId,
+            measurementId: jsonobj.measurementId
+        };
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
 
-            // Retrieve Firebase Messaging object.
-            const messaging = firebase.messaging();
+        // Retrieve Firebase Messaging object.
+        const messaging = firebase.messaging();
 
-            messaging
-                .requestPermission()
-                .then(function () {
-                    //MsgElem.innerHTML = "Notification permission granted." 
-                    console.log("Notification permission granted.");
+        messaging
+            .requestPermission()
+            .then(function () {
+                //MsgElem.innerHTML = "Notification permission granted." 
+                console.log("Notification permission granted.");
 
-                    // get the token in the form of promise
-                    return messaging.getToken()
-                })
-                .then(function (token) {
-                    //TokenElem.innerHTML = "token is : " + token
-                    //console.log("token", token);
-                    var obj = {
-                        User_Id: window.localStorage['UserId'],
-                        FCMToken: token,
-                        DeviceType: "web"
-                    };
-                    
-
-                    var usertoken = '';
-                    usertoken = window.localStorage['dFhNCjOpdzPNNHxx54e+0w=='];
-                    $.ajax({
-                        url: baseUrl + '/api/SendEmail/UpdateUser_FCMTocken',
-                        type: "POST",
-                        beforeSend: function (xhr) {
-                            xhr.setRequestHeader("Authorization", "Bearer " + usertoken);
-                        },
-                        data: obj,
-                        dataType: "json"
-                    })
-                })
-                .catch(function (err) {
-                    //ErrElem.innerHTML =  ErrElem.innerHTML + "; " + err
-                    console.log("Unable to get permission to notify.", err);
-                });
-
-            // [START refresh_token]
-            // Callback fired if Instance ID token is updated.
-            messaging.onTokenRefresh(() => {
-                messaging.getToken().then((refreshedToken) => {
-                    console.log('Token refreshed.', refreshedToken);
-                    var obj = {
-                        User_Id: window.localStorage['UserId'],
-                        FCMToken: refreshedToken,
-                        DeviceType: "web"
-                    };
-
-                    var usertoken = '';
-                    usertoken = window.localStorage['dFhNCjOpdzPNNHxx54e+0w=='];
-                    $.ajax({
-                        url: baseUrl + '/api/SendEmail/UpdateUser_FCMTocken',
-                        type: "POST",
-                        beforeSend: function (xhr) {
-                            xhr.setRequestHeader("Authorization", "Bearer " + usertoken);
-                        },
-                        data: obj,
-                        dataType: "json"
-                    })
-
-                    // [END_EXCLUDE]
-                }).catch((err) => {
-                    console.log('Unable to retrieve refreshed token ', err);
-                    //showToken('Unable to retrieve refreshed token ', err);
-                });
-            });
-            messaging.onMessage((payload) => {
-                //console.log('Message received. ', payload);
-
-                var notificationOptions = {
-                    body: payload.notification.body,
-                    icon: payload.notification.icon,
-                    click_action: payload.notification.click_action
+                // get the token in the form of promise
+                return messaging.getToken()
+            })
+            .then(function (token) {
+                //TokenElem.innerHTML = "token is : " + token
+                //console.log("token", token);
+                var obj = {
+                    User_Id: window.localStorage['UserId'],
+                    FCMToken: token,
+                    DeviceType: "web"
                 };
-                new Notification(payload.notification.title, notificationOptions);
 
+
+                var usertoken = '';
+                usertoken = window.localStorage['dFhNCjOpdzPNNHxx54e+0w=='];
+                $.ajax({
+                    url: baseUrl + '/api/SendEmail/UpdateUser_FCMTocken',
+                    type: "POST",
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer " + usertoken);
+                    },
+                    data: obj,
+                    dataType: "json"
+                })
+            })
+            .catch(function (err) {
+                //ErrElem.innerHTML =  ErrElem.innerHTML + "; " + err
+                console.log("Unable to get permission to notify.", err);
             });
+
+        // [START refresh_token]
+        // Callback fired if Instance ID token is updated.
+        messaging.onTokenRefresh(() => {
+            messaging.getToken().then((refreshedToken) => {
+                console.log('Token refreshed.', refreshedToken);
+                var obj = {
+                    User_Id: window.localStorage['UserId'],
+                    FCMToken: refreshedToken,
+                    DeviceType: "web"
+                };
+
+                var usertoken = '';
+                usertoken = window.localStorage['dFhNCjOpdzPNNHxx54e+0w=='];
+                $.ajax({
+                    url: baseUrl + '/api/SendEmail/UpdateUser_FCMTocken',
+                    type: "POST",
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer " + usertoken);
+                    },
+                    data: obj,
+                    dataType: "json"
+                })
+
+                // [END_EXCLUDE]
+            }).catch((err) => {
+                console.log('Unable to retrieve refreshed token ', err);
+                //showToken('Unable to retrieve refreshed token ', err);
+            });
+        });
+        messaging.onMessage((payload) => {
+            //console.log('Message received. ', payload);
+
+            var notificationOptions = {
+                body: payload.notification.body,
+                icon: payload.notification.icon,
+                click_action: payload.notification.click_action
+            };
+            new Notification(payload.notification.title, notificationOptions);
 
         });
+
+    });
