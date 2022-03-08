4ba32e52ee6cea8de96a1becb40d125020baf85d
Bhuvana <bhuvana@vjhsoftware.in>
2022-03-08 13:45:59 +0530

    Added medication in My Pill Box and ICD10 Warning fixed in Login page

--- MyCortex/scripts/app/controllerLogin.js
+++ MyCortex/scripts/app/controllerLogin.js
@@ -187,7 +187,7 @@ MyCortexControllers.controller("LoginController", ['$scope', '$http', '$routePar
         $scope.usernamekeypress = function (e) {
             var charCode = e.charCode || e.keyCode || e.which;
             if ((charCode >= 33 && charCode <= 39) || (charCode >= 40 && charCode <= 45) || (charCode >= 47 && charCode <= 43) || (charCode >= 58 && charCode <= 63) || (charCode >= 91 && charCode <= 96) || (charCode >= 123 && charCode <= 126)) {
-                toastr.warning("Numbers & Special characters are not allowed except @ and dot.", "warning");
+                toastr.warning("Special characters are not allowed except @ and dot.", "warning");
                 //swal.fire("Numbers & Special characters are not allowed.");
                 if (e.preventDefault) {
                     e.preventDefault();
