8181436d16be7fbf1292faa732a8850d00df0d38
manikandan4423 <mani@vjhsoftware.in>
2022-03-23 15:46:51 +0530

    added appointment date in cg appointment approval page, highlight sub menu in cg login

--- MyCortex/scripts/app/AppointmentApprovalController.js
+++ MyCortex/scripts/app/AppointmentApprovalController.js
@@ -147,8 +147,10 @@ AppointmentApprovalController.controller("AppointmentApprovalController", ['$sco
             }).then((result) => {
                 if (result.isConfirmed) {
                     $scope.Cancelled_Remarks = "";
+                    $scope.ReasonTypeId = '0';
                     $scope.Appointment_Id = id;
                     angular.element('#PatientAppointmentModal').modal('show');
+                    setTimeout(() => { setres(); }, 500);
                 } else if (result.isDenied) {
                 }
             });
