776741539c4f57ecd5f0a6cd65b5d85c7491d04d
Gurumoorthy <gurumoorthy@vjhsoftware.in>
2022-05-25 10:39:35 +0530

    Subscription,Allergies - DatePicker Fixed

--- MyCortex/scripts/app/UserHealthDataDetailsController.js
+++ MyCortex/scripts/app/UserHealthDataDetailsController.js
@@ -7007,7 +7007,7 @@ UserHealthDataDetails.controller("UserHealthDataDetailsController", ['$scope', '
 
                     var obj = {
                         Id: $scope.Id,
-                        OnSetDate: $scope.OnSetDate,
+                        OnSetDate: moment($scope.OnSetDate).format('DD-MMM-YYYY'),
                         AllergenId: $scope.AllergenId,
                         AllergyTypeId: $scope.AllergyTypeId,
                         AllergyOnsetId: $scope.AllergyOnsetId == 0 ? null : $scope.AllergyOnsetId,
