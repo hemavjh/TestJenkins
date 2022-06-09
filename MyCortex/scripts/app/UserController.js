daf675726ebdc75d40a9315f188b44f9f23ce3b4
Manikandan <mani@vjhsoftware.in>
2022-06-09 19:28:01 +0530

    solved - While editing User, not all the values are showing up

--- MyCortex/scripts/app/UserController.js
+++ MyCortex/scripts/app/UserController.js
@@ -3128,7 +3128,10 @@ Usercontroller.controller("UserController", ['$scope', '$q', '$http', '$filter',
                             $scope.resumedoc = data.FILE_NAME;
                             $scope.File_FullPath = data.FILE_FULLPATH;
                             $scope.Upload_FileName = data.UPLOAD_FILENAME;
-                            // $scope.GenderId = data.GENDER_ID.toString();
+                            $scope.GenderId = data.GENDER_ID.toString();
+                            $http.get(baseUrl + '/api/Common/GenderList/').success(function (data) {
+                                $scope.GenderList = data;
+                            });
                             //if ($scope.GenderId != "0" || $scope.GenderId != "") {
                             //    $('#divGender').removeClass("ng-invalid");
                             //    $('#divGender').addClass("ng-valid");
@@ -3138,6 +3141,9 @@ Usercontroller.controller("UserController", ['$scope', '$q', '$http', '$filter',
                             //    $('#divGender').addClass("ng-invalid");
                             //}
                             $scope.NationalityId = data.NATIONALITY_ID.toString();
+                            $http.get(baseUrl + '/api/Common/NationalityList/').success(function (data) {
+                                $scope.NationalityList = data;
+                            });
                             //if ($scope.NationalityId != "0" || $scope.NationalityId != "") {
                             //    $('#divNationality').removeClass("ng-invalid");
                             //    $('#divNationality').addClass("ng-valid");
@@ -3147,6 +3153,13 @@ Usercontroller.controller("UserController", ['$scope', '$q', '$http', '$filter',
                             //    $('#divNationality').addClass("ng-invalid");
                             //}
                             $scope.EthnicGroupId = data.ETHINICGROUP_ID.toString();
+                            $http.get(baseUrl + '/api/Common/EthnicGroupList/').success(function (data) {
+                                $scope.EthnicGroupListTemp = [];
+                                $scope.EthnicGroupListTemp = data;
+                                $scope.EthnicGroupList = angular.copy($scope.EthnicGroupListTemp);
+                                //validation checking for ethnic group
+                                //$scope.PatientEthnicChange();
+                            });
                             //if ($scope.EthnicGroupId != "0" || $scope.EthnicGroupId != "") {
                             //    $('#divPatientEthnic').removeClass("ng-invalid");
                             //    $('#divPatientEthnic').addClass("ng-valid");
@@ -3234,6 +3247,11 @@ Usercontroller.controller("UserController", ['$scope', '$q', '$http', '$filter',
                                 });
                             }
                             $scope.MaritalStatusId = data.MARITALSTATUS_ID.toString();
+                            $http.get(baseUrl + '/api/Common/MaritalStatusList/').success(function (data) {
+                                $scope.MaritalStatusListTemp = [];
+                                $scope.MaritalStatusListTemp = data;
+                                $scope.MaritalStatusList = angular.copy($scope.MaritalStatusListTemp);
+                            });
                             //if ($scope.MaritalStatusId != "0" || $scope.MaritalStatusId != "") {
                             //    $('#divPatientMarital').removeClass("ng-invalid");
                             //    $('#divPatientMarital').addClass("ng-valid");
@@ -3243,6 +3261,11 @@ Usercontroller.controller("UserController", ['$scope', '$q', '$http', '$filter',
                             //    $('#divPatientMarital').addClass("ng-invalid");
                             //}
                             $scope.BloodGroupId = data.BLOODGROUP_ID.toString();
+                            $http.get(baseUrl + '/api/Common/BloodGroupList/').success(function (data) {
+                                $scope.BloodGroupListTemp = [];
+                                $scope.BloodGroupListTemp = data;
+                                $scope.BloodGroupList = angular.copy($scope.BloodGroupListTemp);
+                            });
                             //if ($scope.BloodGroupId != "0" || $scope.BloodGroupId != "") {
                             //    $('#divPatientBlood').removeClass("ng-invalid");
                             //    $('#divPatientBlood').addClass("ng-valid");
