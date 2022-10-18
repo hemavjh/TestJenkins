82d53d4cff345c660dfc82c53eddbd4498903f23
Hemalatha <hema@vjhsoftware.in>
2022-10-18 19:05:41 +0530

    liveboxcontroller parameter issue

--- MyCortex/Repositories/IUserRepository.cs
+++ MyCortex/Repositories/IUserRepository.cs
@@ -150,6 +150,7 @@ namespace MyCortex.Repositories
         long UpdateUserLanguage(long UserId, long LanguageId);
         int Save_User_Eligiblity_Logs(string eligibiltyId, int patient_id, responseModel Obj);
         int Save_User_Appointment_Eligiblity_Logs(int appointment_id, int patient_id, string eligibiltyId, responseAppointmentModel Obj);
-        int Save_Video_Call_Recording_Logs(string conference_id, string recording_url);
+        //int Save_Video_Call_Recording_Logs(string conference_id, string recording_url);
+        int Save_Video_Call_Recording_Logs(string conference_id, string recording_url, string Recordingurl);
     }
 }
