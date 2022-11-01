5daf10121f67ff68a0b630d0a58fc98dacde6d58
vigneshnb <vignesh.babu@vjhsoftware.in>
2022-11-01 12:33:41 +0530

    connection string changes

--- MyCortexDB/ClsDataBase.cs
+++ MyCortexDB/ClsDataBase.cs
@@ -458,8 +458,8 @@ namespace MyCortexDB
         {
 
             //return System.Configuration.ConfigurationManager.ConnectionStrings["Connection"].ToString();
-            //return DBConnectionStringBuilder.ConnectionString;
-            return "server=SQL5080.site4now.net;database=DB_A66DEE_mycortexdemo;uid=DB_A66DEE_mycortexdemo_admin;password=vjh@0304;";
+            return DBConnectionStringBuilder.ConnectionString;
+            //return "server=SQL5080.site4now.net;database=DB_A66DEE_mycortexdemo;uid=DB_A66DEE_mycortexdemo_admin;password=vjh@0304;";
         }
         #endregion
 
