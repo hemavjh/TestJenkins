ebd30db430a698f72fdfcf1d662150e5413e8eaa
vigneshnb <vignesh.babu@vjhsoftware.in>
2022-12-12 14:32:02 +0530

    waitinguserstatus changed to liveboxnotify

--- MyCortex/Home/Controllers/HomeController.cs
+++ MyCortex/Home/Controllers/HomeController.cs
@@ -932,7 +932,14 @@ namespace MyCortex.Home.Controllers
                 {
                     retid = liveBoxRepository.LiveBox_Recording_url(conferencename, recording_url);
                 }
-                retid = liveBoxRepository.LiveBox_Notify_UPDATE(conferencename, InstitutionId,userID);
+                //retid = liveBoxRepository.LiveBox_Notify_UPDATE(conferencename, InstitutionId,userID);
+                Console.WriteLine("0");
+                if (json.Contains("WaitingUserStatus"))
+                {
+                    Console.WriteLine("00");
+                    userID = JObject.Parse(json)["userid"].ToString();
+                    retid = liveBoxRepository.LiveBox_Notify_UPDATE(conferencename, InstitutionId, userID);
+                }
 
                 //PushNotificationMessage message = new PushNotificationMessage();
                 //message.Title = "Notification For Call";
